2ad0d4411142b87cdfc38f0f66cbca3277f5a092
gwanuk <guk1502012@gmail.com>
2023-07-07 01:58:55 +0900

    FEAT:Add handler config

--- src/main/java/com/matzip/thread/security/configs/SecurityConfig.java
+++ src/main/java/com/matzip/thread/security/configs/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.matzip.thread.security.configs;
 
 import com.matzip.thread.security.filter.ApiLoginProcessingFilter;
+import com.matzip.thread.security.handler.ApiAuthenticationFailureHandler;
+import com.matzip.thread.security.handler.ApiAuthenticationSuccessHandler;
 import com.matzip.thread.security.provider.ApiAuthenticationProvider;
 import com.matzip.thread.security.service.CustomUserDetailsService;
 import com.matzip.thread.users.application.port.in.UserUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +17,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -47,6 +49,8 @@ public class SecurityConfig {
         ApiLoginProcessingFilter apiLoginProcessingFilter = new ApiLoginProcessingFilter();
         apiLoginProcessingFilter.setAuthenticationManager(authenticationConfiguration.getAuthenticationManager());
 //        apiLoginProcessingFilter.setAuthenticationManager(authenticationManager());
+        apiLoginProcessingFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+        apiLoginProcessingFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
         return apiLoginProcessingFilter;
     }
 
@@ -75,5 +79,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new ApiAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new ApiAuthenticationFailureHandler();
     }
 }
