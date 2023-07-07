03a7fbf95a3bc704e4690d702e8797ad17d539d7
gwanuk <guk1502012@gmail.com>
2023-07-08 00:36:49 +0900

    FEAT:Add user details service

---
+++ src/main/java/com/matzip/thread/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.matzip.thread.security.service;
+
+import com.matzip.thread.users.application.port.in.UserUseCase;
+import com.matzip.thread.users.domain.User;
+import com.matzip.thread.security.model.UserContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserUseCase userUseCase;
+
+    /**
+     * 유저가 존재하면 UserContext 객체에 User 객체와 GrantedAuthority 리스트를 담아서 리턴
+     * @param username the username identifying the user whose data is required.
+     * @return UserContext implement UserDetails
+     * @throws UsernameNotFoundException UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userUseCase.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("The user does not exist: " + username));
+
+        List<SimpleGrantedAuthority> roles = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new UserContext(user, roles);
+    }
+}
