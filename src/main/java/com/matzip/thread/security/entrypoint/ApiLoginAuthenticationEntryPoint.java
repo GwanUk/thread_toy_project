6183833912570b572aa8bb21ea1224a8a378672c
gwanuk <guk1502012@gmail.com>
2023-07-07 15:06:04 +0900

    FEAT:Add API login Authentication entry point

---
+++ src/main/java/com/matzip/thread/security/entrypoint/ApiLoginAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.matzip.thread.security.entrypoint;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ApiLoginAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().print("Only authorized users have access");
+    }
+}
