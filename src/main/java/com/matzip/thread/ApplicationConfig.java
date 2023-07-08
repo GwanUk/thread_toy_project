892a13ebc7810001c35f42729bfef59584935e81
gwanuk <guk1502012@gmail.com>
2023-07-08 21:32:00 +0900

    auditing 익명 사용자 버그 수정

--- src/main/java/com/matzip/thread/ApplicationConfig.java
+++ src/main/java/com/matzip/thread/ApplicationConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -15,6 +17,14 @@ public class ApplicationConfig {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.ofNullable(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication instanceof AnonymousAuthenticationToken) {
+                return Optional.of((String) authentication.getPrincipal());
+            }
+
+            return Optional.ofNullable(((User) authentication.getPrincipal()).getUsername());
+        };
     }
 }
