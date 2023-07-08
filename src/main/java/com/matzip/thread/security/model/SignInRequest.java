a820273259468d80edc9db67d6a48363d7df1ddc
gwanuk <guk1502012@gmail.com>
2023-07-08 14:38:34 +0900

    불변객체로 변경

--- src/main/java/com/matzip/thread/security/model/SignInRequest.java
+++ src/main/java/com/matzip/thread/security/model/SignInRequest.java
@@ -1,16 +1,13 @@
 package com.matzip.thread.security.model;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignInRequest {
 
-    private String username;
-    private String password;
+    private final String username;
+    private final String password;
 
     public SignInRequest(String username, String password) {
         this.username = username;
