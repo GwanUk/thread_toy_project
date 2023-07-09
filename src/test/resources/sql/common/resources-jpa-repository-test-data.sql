715f73b52c6196818a8b2d74d442273e651ab2f9
gwanuk <guk1502012@gmail.com>
2023-07-09 21:38:51 +0900

    add resource test sql script

---
+++ src/test/resources/sql/common/resources-jpa-repository-test-data.sql
@@ -0,0 +1,14 @@
+insert into RESOURCES (resources_id, uri)
+values (1, '/api/thread')
+insert into RESOURCES (resources_id, uri)
+values (2, '/api/admin')
+
+insert into RESOURCES_ROLES (resources_roles_id, resources_id, roles_id, order_num)
+values (1, 1, 1, 1)
+insert into RESOURCES_ROLES (resources_roles_id, resources_id, roles_id, order_num)
+values (2, 2, 2, 1)
+
+insert into ROLES (roles_id, role_name, description)
+values (1, "ROLE_USER", "유저 권한")
+insert into ROLES (roles_id, role_name, description)
+values (1, "ROLE_ADMIN", "관리자 권한")
